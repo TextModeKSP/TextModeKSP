1293de8ff1e6c6fc4056d6252a2b79bc2ec8950d
AwesomeDerek <derekzhou2003@gmail.com>
2016-02-22 13:58:47 +0800

    Edison is a human

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.awt.*;
+
+import javax.swing.*;
+
 public class Main {
 	
 	static long memAvailableMiB = Runtime.getRuntime().totalMemory() / 1048576;
@@ -11,6 +15,17 @@ public class Main {
 		System.out.println("System information:");
 		System.out.println("Available processors (cores): " + Runtime.getRuntime().availableProcessors());
 	    System.out.println("Total memory available to JVM (MiB): " + Main.memAvailableMiB);
+		
+	}
+	
+	public newWindow extends Frame {
+		
+			setTitle("Text Mode KSP"); // Set the title
+		    setSize(400,400); // Set size to the frame
+		    setVisible(true); // Make the frame visible
+		    setBackground(Color.red); // Set the background
+		    setExtendedState(MAXIMIZED_BOTH); // Make the frame maximized
+		    setCursor(Cursor.HAND_CURSOR); // Deprecated
 		
 	}
 	
---
+++ src/main/Window.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import main.Main;
+
+
+
+public class Window
+{
+		public static void Window(String args[]) {
+			
+			new createWindow();
+			
+		}
+}
